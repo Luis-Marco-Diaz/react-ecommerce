5d6b2231c5ca304bd45879b75136055e122fbee9
Marco <marko.dzm@gmail.com>
2023-05-16 13:32:28 -0600

    Avances

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,31 +2,54 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 
-const Location = () => {
+const Location = ({newId}) => {
+    
+        const [Id2, setId2, ] = useState("");
+            const onsubmit = (e) => {
+            e.preventDefault();
+            console.log({Id});
+            if(Id2 === "" || Id2.toString) return;
+            newId(Id2);
+            } 
 
+    
+    
     const [ Id, setId ] = useState({});
 
-
     useEffect(() => {
-        axios.get('https://rickandmortyapi.com/api/location/3')
-             .then(res => setId(res.data));
+        axios.get(`https://rickandmortyapi.com/api/location/${Id2}`)
+             .then(res => setId(res.data))
+             .catch(error => console.error(error))
+             
     }, [])
+
 
     return (
 
-        <div className='Location-Data'>
-            <div>
+        <div>
+            <div className="form" >
+            <form onSubmit={onsubmit}>
+                <div>
+                    <input type="text" placeholder="Type a location Id (1 to 101)" onChange={(e) =>setId2(e.target.value)}/>
+                    <button type="submit">Buscar por Id</button>
+                </div>
+             </form>
+            </div>
+            <div className='Location-Data'>
+             <div>
                 <h3> Name: <br /> {Id.name} </h3>
-            </div>
-            <div>
+              </div>
+              <div>
                 <h3> Type: <br /> {Id.type} </h3>
-            </div>
-            <div>
+              </div>
+              <div>
                 <h3> Dimension: <br /> {Id.dimension} </h3>
-            </div>
-            <div>
+             </div>
+             <div>
                 <h3> Population: <br /> {(Id.residents)?.length} </h3>
             </div>
+             </div>
+            
         </div>
 
     );
