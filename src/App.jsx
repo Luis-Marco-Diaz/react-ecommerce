23f22242d413e423b2aa645bdd32cdbe59b3e154
Marco <marko.dzm@gmail.com>
2023-05-29 14:17:41 -0600

    Nuevo iniciio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import reactLogo from './assets/react.svg'
-import LogoRick from '/logoRickandMorty.svg'
+
 import './App.css'
-import Location from './components/Location'
+
 
 function App() {
 
@@ -10,18 +9,9 @@ function App() {
       <div>
 
         <header>
-        <a href="https://github.com/Luis-Marco-Diaz/RickandMorty" target="_blank">
-          <img src={LogoRick} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
         <h2>Rick and Morty World Search</h2>
         </header>
 
-        <div className='Content' >
-          <Location/>
-        </div>
 
       </div>
   )
