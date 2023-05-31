25a582174d7d7037228f1a8e6629b83057cada1c
Marco <marko.dzm@gmail.com>
2023-05-31 14:21:58 -0600

    CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-
 import './App.css'
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import UsersForm from './components/UsersForm';
+import UsersList from './components/UsersList';
 
 function App() {
+
+  const [UserList, setUserList] = useState({});
+
+  const getData = () => {
+    axios
+      .get("'https://users-crud.academlo.tech/users/'")
+      .then( (resp) => 
+        setUserList(resp.data),
+        console.log(setUserList))
+      .catch( (error) => console.error(error));
+  };
 
 
   return (
       <div>
-
         <header>
-        <h2>Rick and Morty World Search</h2>
+        <h2>Users CRUD</h2>
         </header>
-
+        <div>
+          <UsersList
+          userList = { userList }
+          deleteUser = { deleteUser }
+          />
+          <UsersForm/>
+        </div>
 
       </div>
   )
---
+++ src/components/UsersForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersForm = () => {
+    return (
+        <div>
+            <h1>User List</h1>
+        </div>
+    )
+};
+
+export default UsersForm
