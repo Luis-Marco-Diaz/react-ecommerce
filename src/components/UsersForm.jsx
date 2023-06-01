914193cdec1feaa7eb26e29cbc90e1e1d1ff253b
Marco <marko.dzm@gmail.com>
2023-06-01 11:04:00 -0600

    CRUD-A

--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useEffect } from 'react'
 
 const UsersForm = () => {
+    
+    const { register, handleSubmit, reset } = useForm()
+
+    const emptyUser = {
+        fist_name : "",
+        last_name : "",
+        birthday : "",
+        password : "",
+    }
+
     return (
         <div>
             <h1>User List</h1>
