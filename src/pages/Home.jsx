91819f40d0643779278a352ff578c2788bd4f821
Marco Diaz <123221592+Luis-Marco-Diaz@users.noreply.github.com>
2023-06-27 15:13:53 +0000

    Container and rows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,41 +8,55 @@ import Form from "react-bootstrap/Form";
 function Home() {
   return (
     <div>
-      <Row className="pt-5" > 
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://random.imagecdn.app/500/150" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://random.imagecdn.app/500/150" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://random.imagecdn.app/500/150" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-        </Card>
-        </Row>
+      <Row className="pt-5" >
+        <Col lg={4} md={3} >
+        Filtrados
+        </Col>
+        <Col lg={8} md={9} >
+          Productos
+          <Row>
+              <Col>
+                  <Card style={{ width: '16rem' }}>
+                    <Card.Img variant="top" src="https://random.imagecdn.app/500/150" />
+                      <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                          Some quick example text to build on the card title and make up the
+                          bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                      </Card.Body>
+                  </Card>
+                </Col>
+                <Col>
+                <Card style={{ width: '16rem' }}>
+                  <Card.Img variant="top" src="https://random.imagecdn.app/500/150" />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                      </Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card style={{ width: '16rem' }}>
+                  <Card.Img variant="top" src="https://random.imagecdn.app/500/150" />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                      </Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+              </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   )
 }
