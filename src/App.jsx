f51c46407a0cb0d8b696f2a16eb6ea5dabb9ed9c
Marco Diaz <123221592+Luis-Marco-Diaz@users.noreply.github.com>
2023-06-27 03:23:41 +0000

    Update 5 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import './App.css'
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {useSelector} from "react-redux";
 import AppNavbar from './components/AppNavbar';
 import Home from './pages/Home';
-import Login from './pages/login';
-import Dynamind from './pages/dynamind';
-import DynamindSport from './pages/dynamindSport';
-import Noopept from './pages/noopept';
-import Purchases from './pages/purchases';
+import Login from './pages/Login';
+import Dynamind from './pages/Dynamind';
+import DynamindSport from './pages/DynamindSport';
+import Noopept from './pages/Noopept';
+import Purchases from './pages/Purchases';
 import ProductsDetail from './pages/ProductsDetail';
 import Loader from './components/Loader';
 
 function App() {
-
+  
+  const isLoading = useSelector((state) => state.isLoading);
+  
   return (
     <HashRouter>
-      <Loader/>
+      {isLoading && <Loader />}
       <AppNavbar/>
       <Routes>
             <Route element = { <Home/> } path='/'/>
