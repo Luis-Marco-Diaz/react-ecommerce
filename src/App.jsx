71fb6200e106e3a448d2ac0f955986d11cec1a42
Marco Diaz <123221592+Luis-Marco-Diaz@users.noreply.github.com>
2023-11-29 14:12:27 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,39 @@
-import './App.css'
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {useSelector} from "react-redux";
-import AppNavbar from './components/AppNavbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Dynamind from './pages/Dynamind';
-import DynamindSport from './pages/DynamindSport';
-import Noopept from './pages/Noopept';
-import Purchases from './pages/Purchases';
-import ProductsDetail from './pages/ProductsDetail';
-import Loader from './components/Loader';
-import Container from 'react-bootstrap/Container';
+import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useSelector } from "react-redux";
+import AppNavbar from "./components/AppNavbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dynamind from "./pages/Dynamind";
+import DynamindSport from "./pages/DynamindSport";
+import Noopept from "./pages/Noopept";
+import Purchases from "./pages/Purchases";
+import ProductsDetail from "./pages/ProductsDetail";
+import Loader from "./components/Loader";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  
   const isLoading = useSelector((state) => state.isLoading);
-  
+
   return (
     <HashRouter>
       {isLoading && <Loader />}
-      <AppNavbar/>
-        <Container fluid>
-          <Routes>
-                <Route element = { <Home/> } path='/'/>
-                <Route element = { <Login/> } path='/Login'/>
-                <Route element = { <Dynamind/> } path='/Dynamind'/>
-                <Route element = { <DynamindSport/> } path='/DynamindSport'/>
-                <Route element = { <Noopept/> } path='/Noopept'/>
-                { /* Protected Routes*/ }
-                <Route element = { <Purchases/> } path='/Purchases'/>
-                <Route element = { <ProductsDetail/> } path='/ProductsDetail/:id'/>
-          </Routes>
-        </Container>
+      <AppNavbar />
+      <Container fluid>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Login />} path="/Login" />
+          <Route element={<Dynamind />} path="/Dynamind" />
+          <Route element={<DynamindSport />} path="/DynamindSport" />
+          <Route element={<Noopept />} path="/Noopept" />
+          {/* Protected Routes*/}
+          <Route element={<Purchases />} path="/Purchases" />
+          <Route element={<ProductsDetail />} path="/ProductsDetail/:id" />
+        </Routes>
+      </Container>
     </HashRouter>
   );
 }
 
 export default App;
-
-
